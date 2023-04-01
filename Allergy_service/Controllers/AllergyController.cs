33769bcf2fe987be4b53c42d61dcbbd4099cfd0d
abdulaleem01 <102243797+abdulaleem01@users.noreply.github.com>
2023-04-01 18:18:39 +0530

    docker compose created for gatewayapi

---
+++ Allergy_service/Controllers/AllergyController.cs
@@ -0,0 +1,39 @@
+﻿using Allergy_Business_Logic;
+using EntityApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Capstone_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllergyController : Controller
+    {
+        ILogic logic;
+        public AllergyController(ILogic logic) => this.logic = logic;
+
+        [HttpGet("Fetch/{VisitId}")]
+        public IActionResult Get([FromRoute] int VisitId)
+        {
+            try
+            {
+                var allergy = logic.Get(VisitId);
+                return Ok(allergy);
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("Add")]
+        public IActionResult Add([FromBody] Models.Allergy allergy)
+        {
+            var ne = logic.AddDetails(allergy);
+            return Created("Add", ne);
+        }
+    }
+}
---
+++ Email_Service/Controllers/Email_Sender.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Email_Service.Controllers
+{
+    public class Email_Sender
+    {
+        public void Email_Method(string email, int status)
+        {
+            string subject = "";
+            string body = "";
+
+
+            switch(status)
+            {
+                case 0:
+                    subject = "Your appointment status of Poseidon healthcare";
+                    body = "Your appointment for visit poseidon health care is booked successfully.\nPlease wait for doctor to accept your appointment";
+                    break;
+                case 1:
+                    subject = "Your appointment status of Poseidon healthcare";
+                    body = "Your Appointment for visit poseidon health care is accepted.\nKindly check your status on Poseidon Healthcare website";
+                    break;
+                case 4:
+                    subject = "Your appointment status of Poseidon healthcare";
+                    body = "Sorry to inform you.Your Appointment for visit poseidon health care is Rejected.\nPlease book other appointment";
+                    break;                    
+            }
+            MailMessage message = new MailMessage();
+
+            message = new MailMessage("yuvamanikandan15@gmail.com", email);
+
+            message.Subject = subject;
+            message.Body = body;
+
+            SmtpClient client = new SmtpClient();
+
+            client.Host = "smtp.gmail.com";
+
+            client.Port = 587;
+
+            client.Credentials = new NetworkCredential("healthcareposeidon@gmail.com", "xqllhshwscxxwqjp");
+
+            client.UseDefaultCredentials = false;
+
+            client.EnableSsl = true;
+
+            try
+            {
+                client.Send(message);
+                Console.WriteLine("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending email: " + ex.Message);
+            }
+
+            message.Dispose();
+            client.Dispose();
+
+        }
+    }
+}
